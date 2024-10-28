e0f6707cd84c1452f50c1f5d6058ef5441969dd2
Martin Erpicum <martin.erpicum@cfwb.be>
2024-10-28 09:24:37 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
-# tax_brakets_belgium
+# Tax Brackets Belgium
 
-experimentations based on https://www.lecho.be/economie-politique/belgique/federal/formation-du-gouvernement-federal-bart-de-wever-veut-supprimer-la-tranche-superieure-d-impots/10569767.html
+This repository contains experimentations based on [this article](https://www.lecho.be/economie-politique/belgique/federal/formation-du-gouvernement-federal-bart-de-wever-veut-supprimer-la-tranche-superieure-d-impots/10569767.html).
 
-## results
+## imposition.py
+
+`imposition.py` is a script that calculates the tax brackets for Belgium. It processes income data and applies the relevant tax rates to determine the tax owed. The script is designed to help understand the impact of proposed changes to the tax system.
+
+## Results
+
 ![imposition.png](imposition.png)
--- imposition.py
+++ imposition.py
@@ -6,7 +6,7 @@ import os
 # Define tax brackets
 current_tax_brackets = {
     "default_rate": 0.50,
-    "description": "Tranches d'imposition actuelles (revenus 2024 impots 2025)",
+    "description": "Current tax brackets (2024 income, 2025 taxes)",
     "instalments": [
         {"amount": 15820, "rate": 0.25},
         {"amount": 12100, "rate": 0.40},
@@ -16,7 +16,7 @@ current_tax_brackets = {
 
 new_tax_brackets = {
     "default_rate": 0.45,
-    "description": "Nouvelles tranches d'imposition basées sur la proposition de Bart De Wever (N-VA)",
+    "description": "New tax brackets based on Bart De Wever's proposal (N-VA)",
     "instalments": [
         {"amount": 16000, "rate": 0.25},
         {"amount": 5000, "rate": 0.35},
@@ -44,75 +44,61 @@ def calculate_total_tax(revenue, tax_brackets) -> float:
     total_tax += remaining_revenue * tax_brackets['default_rate']
     return total_tax
 
-def generate_tax_report(revenues, systems) -> str:
+def generate_tax_report(revenues, systems) -> pd.DataFrame:
     """
     Generate tax data for given revenues and taxation systems.
     """
-    data = "system_name1,tax1,system_name2,tax2,revenue\n"
+    data = []
     for revenue in revenues:
+        row = {'revenue': revenue}
         for system_name, system in systems.items():
             tax = calculate_total_tax(revenue, system)
-            data += f"{system_name},{tax},"
-        data += f"{revenue}\n"
-    return data
+            row[f'{system_name}_tax'] = tax
+        data.append(row)
+    return pd.DataFrame(data)
 
-def save_report_to_csv(data, file_path) -> None:
+def enrich_data(df) -> pd.DataFrame:
     """
-    Save data to a CSV file.
+    Enrich data from a DataFrame.
     """
-    # delete file if it already exists
-    if os.path.exists(file_path):
-        os.remove(file_path)
-    with open(file_path, 'w') as f:
-        f.write(data)
-        
-def enrich_data(file_path) -> None:
-    """
-    Enrich data from a CSV file.
-    """
-    df = pd.read_csv(file_path)
-    df['rate1'] = df['tax1'] / df['revenue']
-    df['rate2'] = df['tax2'] / df['revenue']
-    df['diff'] = ((df['tax1'] - df['tax2']) / df['revenue']).abs() * 100
-    df.to_csv(file_path, index=False)
-
+    df['rate_super_nota_bart_de_wever_102024'] = df['super_nota_bart_de_wever_102024_tax'] / df['revenue']
+    df['rate_current_system'] = df['current_system_tax'] / df['revenue']
+    df['diff'] = ((df['rate_current_system'] - df['rate_super_nota_bart_de_wever_102024']) / df['rate_current_system']).abs() * 100
+    df.to_csv("./imposition.csv", index=False)
     return df
 
 def plot_tax_report(df, output_image) -> None:
     """
-    Plot tax data from a CSV file and save as an image.
+    Plot tax data from a DataFrame and save as an image.
     """
-
     plt.figure(figsize=(10, 6))
-    plt.plot(df['revenue'], df['rate1'], label='current_system')
-    plt.plot(df['revenue'], df['rate2'], label='super-nota-bart-de-wever')
+    plt.plot(df['revenue'], df['rate_current_system'], label='Current System')
+    plt.plot(df['revenue'], df['rate_super_nota_bart_de_wever_102024'], label='Super Nota Bart De Wever 102024')
     plt.xlabel('Revenue')
-    plt.ylabel('Tax rate')
-    plt.title('Tax rate as a function of revenue')
+    plt.ylabel('Tax Rate')
+    plt.title('Comparison of Tax Bracket Systems in Belgium')
     plt.legend(loc='lower right')
-    # on other axis plot difference between the two systems
-    plt.twinx()
-    
-    plt.plot(df['revenue'], df['diff'], label='difference', color='orange', linestyle='--')
-    plt.ylabel('Difference (%)')
-    
-    # add a line for median revenue at 4,076 euros per month (2022 data statbel)
-    plt.axvline(x=4076 * 12, color='grey', linestyle='--', label='Median revenue')
-    
-    # add legend
+
+    # Plot difference on secondary y-axis
+    ax2 = plt.gca().twinx()
+    ax2.plot(df['revenue'], df['diff'], label='Difference (%)', color='purple', linestyle=':')
+    ax2.set_ylabel('Difference (%)')
+
+    # Add a line for median revenue at 4,076 euros per month (2022 data statbel)
+    plt.axvline(x=4076 * 12, color='green', linestyle='--', label='Median Revenue')
+
+    # Add legend
     plt.legend()
-    
     plt.savefig(output_image)
 
 # Main execution
-revenue_range = range(5000, 200000, 500)
-systems = {
-    "current_system": current_tax_brackets,
-    "super-nota-bart-de-wever": new_tax_brackets
-}
+if __name__ == "__main__":
+    revenue_range = range(5000, 200000, 500)
+    systems = {
+        "current_system": current_tax_brackets,
+        "super_nota_bart_de_wever_102024": new_tax_brackets
+    }
 
-data = generate_tax_report(revenue_range, systems)
-csv_file = "./imposition.csv"
-save_report_to_csv(data, csv_file)
-df = enrich_data(csv_file)
-plot_tax_report(df, 'imposition.png')
+    df = generate_tax_report(revenue_range, systems)
+    df = enrich_data(df)
+    plot_tax_report(df, 'imposition.png')
